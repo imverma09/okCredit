2bdaa4efc98b67bb7153a7391c40b51d396e9e91
imverma09 <imverma45@gmail.com>
2024-08-20 15:38:51 +0530

    fixed user list

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from "react"
 
 function App() {
   const [newChat, setNewChat] = useState(false)
-  const [amountBox, setAmountBox] = useState(false)
   const [userList, setUserList] = useState([])
   const [userData, setUserData] = useState({ userName: "", userAmount: "", option: "receive" })
-  const [payAmount, setPayAmount] = useState(null)
   const [totalR, setTotalR] = useState('00')
   const [totalP, setTotalP] = useState('00')
+  const [showArr ,setShowArr] =useState([])
 
     let amt = null;
   const handleChange = (event) => {
@@ -24,28 +23,42 @@ function App() {
     if(userData.option == "pending"){
       setTotalP(prev => Number(prev) + Number(userData.userAmount) )
     } 
-   
     setUserData({ userName: "", userAmount: "", option: "receive" })
   }
-  function payForm(e) {
-    e.preventDefault()
-    amt = payAmount
-    setPayAmount('')
-    setAmountBox(false)
-    console.log(payAmount)
-  }
+
   function pay(e) {
     const { name, id } = e.target
     if (name == "pay") {
-      setAmountBox(true)
-      console.log(amt)
-      setUserList(users =>users.map((user, idx)=> idx == id ?  {...user, userAmount: Number(user.userAmount) - Number(amt)}: user))
-      console.log(userList)
-      console.log(payAmount)
+       if(amt == null){
+        alert('enter amount')
+        return
+       }
+      setShowArr(prev => prev.filter(val => val !=id ))
+      // setUserList(users =>users.map((user, idx)=> idx == id ?  {...user, userAmount: Number(user.userAmount) - Number(amt)}: user))
+     const newUserList = userList.map((user ,idx)=>{
+          if(idx == id){
+            if (user.userAmount >= amt) {
+              setTotalP(prev => prev - amt)
+              return {...user, userAmount: Number(user.userAmount) - Number(amt)}            
+            }else{
+              console.log(user.userAmount, amt)
+              alert("Enter Correct amount")
+            }
+          }
+          return user
+      })
+      setUserList(newUserList)
     } else {
       const remind = userList.filter((val, i) => i == id)
       alert("Remind to " + remind[0].userName)
     }
+  }
+  function payValue(e){
+      amt  = e.target.value
+    }
+    function addInput(e){
+      const id = e.target.id
+      setShowArr(prev => [...prev , id])
   }
 
   return (
@@ -86,20 +99,23 @@ function App() {
                   <p>{val.userName} </p>
                   <p> ₹ {val.userAmount} </p>
                 </div>
-                <button className=" border-emerald-300 border-2 px-6 mx-4 text-lg cursor-pointer" id={i} name={val.option == "receive" ? "receive" : "pay"} onClick={pay}>{val.option == "receive" ? "Receive" : "Pay"}</button>
+               <div>
+                {
+                  val.option == 'receive'?<>
+                   <button className=" border-emerald-300 border-2 px-6 mx-4 text-lg cursor-pointer" id={i} name={val.option == "receive" ? "receive" : "pay"} onClick={pay}>Receive</button>
+                  </> 
+                  : <>
+                  {
+                    showArr.includes(String(i)) ? <input type="number" placeholder="Enter Amount" onChange={payValue} className="outline-none border-2 border-gray-800 rounded-md px-3" /> : <span className="cursor-pointer" onClick={addInput} id={i}>{"👈"}</span>
+                  }
+                  <button className=" border-emerald-300 border-2 px-6 mx-4 text-lg cursor-pointer" id={i} name={val.option == "receive" ? "receive" : "pay"} onClick={pay}>Pay</button>
+                  </>
+                }
+               </div>
               </div>
             )
           }
         </main>
-        {
-          amountBox && <div className="popup rounded-md">
-            <button className="bg-red-500 px-1 rounded-md" id="delete" onClick={() => { setAmountBox(false) }}>X</button>
-            <form action="" className="flex flex-col gap-4" onSubmit={payForm}>
-              <input type="number" required placeholder="Enter Amount" value={payAmount} onChange={(e) => { setPayAmount(e.target.value) }} className="outline-none border-2 border-gray-800 rounded-md px-3" />
-              <button className="border-solid border-cyan-600 border-2 px-3 bg-slate-300" >Add</button>
-            </form>
-          </div>
-        }
       </section>
     </>
   )
